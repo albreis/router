8284b598a029e71b72f01db7170f3db3ccb74f69
Albreis - Design & Programação <albreisdev@gmail.com>
2021-08-02 09:46:36 -0300

    Adicionado atalho para CLI

---
+++ README.md
@@ -0,0 +1,163 @@
+# Como usar
+
+Antes de tudo precisamos importar nosso loader do composer:
+
+```php
+include 'vendor/autoload.php;
+```
+
+Agora podemos instânciar o Router.
+
+```php
+$router = new \Albreis\Router;
+
+// ou
+
+use Albreis\Router;
+
+$router = new Router;
+
+```
+
+## Fazendo uma requisição
+O Router aceita qualquer método de requisição e possui 3 argumentos:
+
+```php
+$router->[metodo]([path], [callback], [bypass]);
+```
+
+**metodo** GET, POST, DELETE, CLI, etc
+
+**path** é o caminho em que o callback deve ser executado. Usa REGEX para definir as rotas.
+
+**callback** é um callable, seja uma função ou método e aceita os formatos:
+
+- função anônima `function() { ... }`
+- métodos `[$instancia, metodo]` Ex. [$this, run]
+- métodos estáticos `'Classe::metodo'` (caso não seja estático será criada uma instância da classe automaticamente)
+- Arrow functions `fn() => return true`
+
+**bypass** indica se o script vai continuar sendo executado após o match com a rota.
+
+# Exemplos
+
+Exemplo 1:
+```php
+$router->get('^login$', function(){
+    /**
+     * Aqui tu podes criar toda sua lógica seja retornar 
+     * um JSON ou um HTML
+     **/
+});
+```
+
+Exemplo 2:
+```php
+$router->get('^([^/]+)/([^/]+)$', function($category, $post){
+    /**
+     * Aqui tu podes criar toda sua lógica seja retornar 
+     * um JSON ou um HTML
+     **/
+});
+```
+
+O Router chamará a função `exit()` após executar o callback, evitando assim a execução do restante do script.
+
+Caso queira que o script continue sendo executado mesmo após dar match com a rota basta passar o parâmetro **bypass** como **true**.
+
+Exemplo 3:
+```php
+$router->get('^([^/]+)/([^/]+)$', function($category, $post){
+
+    /**
+     * Ao acessar a URL /frutas/melancia
+     * A saída do var_dump abaixo seria:
+     * string(6) "frutas" string(8) "melancia"
+     */
+     var_dump($category, $post);
+
+    /**
+     * Essa rota vai ser executada e continuar para
+     * a próxima rota pois foi passado o parâmetro 3 como true
+     */
+
+}, true);
+```
+Ao acessar a rota http://seusite.com.br/minha-categoria/postagem-109 as variáveis `$category` e `$post` teriam os valores **minha-categoria** e **postagem-109** respectivamente.
+
+# Como criar um middleware
+Para usar o Router como um middleware para setar a rota como **bypass**.
+
+No exemplo abaixo temos um middleware que é executado em todas as requisições do método POST para a rota login.
+
+Um exemplo de uso seria para criar um sistema de logs de requisição ou pré tratamento de dados.
+
+Exemplo 4:
+```php
+$router->get('^login$', function(){
+    /**
+     * Executa as instruções e segue para a próxima rota
+     */
+
+    /**
+     * Este print_r() irá retornar Array ( )
+     */
+    print_r($_GET);
+
+    $_GET['teste'] = 123;
+}, true);
+
+$router->get('^login$', function(){
+    /**
+     * Daqui em diante nada será executado
+     */  
+
+    /**
+     * Este print_r() irá retornar Array ( [teste] => 123 )
+     */
+    print_r($_GET);
+     
+});
+```
+
+# Exemplos avançados
+
+Exemplo 5 (funções anônimas):
+```php
+$router->get('^login$', function(){
+     
+});
+```
+
+Exemplo 6 (arrow functions):
+```php
+$router->get('^([^/]+)/([^/]+)$', fn($a, $b) => var_dump($a, $b));
+```
+
+Exemplo 6 (métodos):
+```php
+class Home {
+    public function index($a, $b) {
+        echo 'Homepage';
+    }
+}
+
+$home = new Home;
+
+$router->get('^([^/]+)/([^/]+)$', [$home, 'index']);
+
+// ou 
+
+$router->get('^([^/]+)/([^/]+)$', 'Home::index');
+
+// ou
+
+$router->get('^([^/]+)/([^/]+)$', function($a, $b) use ($home) {
+    $home->index($a, $b);
+});
+
+// ou
+
+$router->get('^([^/]+)/([^/]+)$', fn($a, $b) => $home->index($a, $b));
+
+```
---
+++ src/Router.php
@@ -0,0 +1,146 @@
+<?php namespace Albreis;
+
+use ReflectionMethod;
+use Exception;  
+
+/**
+ * Class Router
+ * @package Albreis
+ */
+class Router
+{
+    /**
+     * @var array
+     */
+    private $routes = [];
+
+    public $uri;
+
+    public function __construct($uri = null)
+    {
+        $this->uri = $uri;
+        $this->uri();
+    }
+
+    /**
+     * @return string
+     */
+    public function method()
+    {
+        return isset($_SERVER['REQUEST_METHOD']) ? strtolower($_SERVER['REQUEST_METHOD']) : 'cli';
+    }
+
+    /**
+     * @return string
+     */
+    public function uri()
+    {
+
+        if($this->uri) 
+        {
+            return $this->uri;
+        }
+
+        if($this->method() == 'cli') 
+        {
+            global $argv;
+
+            if(!isset($argv[1])) {
+                throw new Exception("URI is required", 1);
+            }
+
+            $arguments = $argv;
+
+            unset($arguments[0]);
+
+            if(isset($argv[2])) {
+                $_SERVER['REQUEST_METHOD'] = array_shift($arguments);
+                $_SERVER['REQUEST_URI'] = array_shift($arguments);
+            }
+            else {
+                $_SERVER['REQUEST_URI'] = array_shift($arguments);
+            }
+
+            if(count($argv) > 3) {
+                parse_str(implode('&', $arguments), $_GET);
+            }
+        }
+
+        $self = isset($_SERVER['PHP_SELF']) ? str_replace('index.php/', '', $_SERVER['PHP_SELF']) : '';
+        $uri = isset($_SERVER['REQUEST_URI']) ? explode('?', $_SERVER['REQUEST_URI'])[0] : '';
+        $uri = str_replace($_SERVER['SCRIPT_NAME'], '/', $uri);
+        $uri = urldecode($uri);
+
+        if ($self !== $uri) {
+            $peaces = explode('/', $self);
+            array_pop($peaces);
+            $start = implode('/', $peaces);
+            $search = '/' . preg_quote($start, '/') . '/';
+            $uri = preg_replace($search, '', $uri, 1);
+        }
+
+        return $uri;
+    }
+
+    /**
+     * is triggered when invoking inaccessible methods in an object context.
+     *
+     * @param $name string
+     * @param $arguments array
+     * @return mixed
+     * @link http://php.net/manual/en/language.oop5.overloading.php#language.oop5.overloading.methods
+     */
+    function __call($name, $arguments)
+    {
+        list($path, $callback, $bypass) = array_pad($arguments, 3, '');
+
+        return $this->exec($name, $path, $callback, $bypass);
+    }
+
+    /**
+     * @param $method
+     * @param $uri
+     * @return mixed|null
+     */
+    private function exec($method, $path, $callback, $bypass = false)
+    {
+        if(is_array($method)) {
+            foreach($method as $m) {
+                $this->exec($m, $path, $callback, $bypass);
+            }
+            return;
+        }
+        $method = strtolower($method);
+        $uri = $path;
+        $pattern = str_replace('/', '\/', $uri);
+        $route = '/' . $pattern . '/';
+        
+        if (($method == $this->method() || $method == '*' || $method == 'all') && preg_match($route, $this->uri(), $parameters)) {
+            array_shift($parameters);
+            $this->call($callback, $parameters);
+            if(!$bypass) {
+                exit;
+            }
+        }
+    }
+
+    /**
+     * @param $callback
+     * @param $parameters
+     * @return mixed
+     */
+    private function call($callback, $parameters)
+    {
+        if(is_string($callback) && count($call = explode('::', $callback)) == 2) {
+            $method = new ReflectionMethod($call[0], $call[1]);
+            if (!$method->isStatic()) {
+                $callback = [new $call[0], $call[1]];
+            }
+        }
+        if (is_callable($callback)) {
+            return call_user_func_array($callback, $parameters);
+        }
+        return null;
+    }
+
+}
